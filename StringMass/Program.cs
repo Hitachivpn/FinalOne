e4ba0d5866af1e51c8789c0a275873e9a1dde8b7
Hitahci <stefan36rus@gmail.com>
2022-11-11 13:19:44 +0300

    Создана блок схема метода фильтрации

--- StringMass/Program.cs
+++ StringMass/Program.cs
@@ -5,11 +5,20 @@ void FilterForThree(string[] array1, string[] array2)
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-    if(array1[i].Length <= 3)
+    if(array1[i].Length < 4)
         {
         array2[count] = array1[i];
         count++;
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
 FilterForThree(array1, array2);
+PrintArray(array2);
