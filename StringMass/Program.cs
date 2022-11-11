b43dca58c727df5c818fadc2a799fd79d3d2deb6
Hitahci <stefan36rus@gmail.com>
2022-11-11 13:09:15 +0300

    Filter for Three length

--- StringMass/Program.cs
+++ StringMass/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[] {"123", "22", "hello", "world", "yes", "no", "long string"};
+string[] array2 = new string[array1.Length];
+void FilterForThree(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+FilterForThree(array1, array2);
